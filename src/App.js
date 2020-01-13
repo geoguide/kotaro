20f0eff5087d28566aad08bcb296a53580fb1328
geoguide <geoffguidetti@gmail.com>
2020-01-13 15:32:05 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Kotaro</h2>
       </header>
+      <nav>
+        <ul>
+          <li>Typical Day</li>
+          <li>TroubleShooting</li>
+          <li>Commands + Hand Signals</li>
+        </ul>
+      </nav>
+      <main>
+        <p>around every hour or whenever you switch modes (hanging out in my room, play in common area, sleeping in crate)  bring him out to pee on leash. Give him at last 5-10 min to make sure he&apos;s going to do it, bring him to grass (or clovers) please. I don&apos;t want him trying to pee on roads and driveways.
+He can&apos;t be in the kitchen/living room unless you&apos; re actively playing with him. My room is okay for him to just be chilling.
+If he gets crazy you can crate him. Generally if you&apos;re near the crate he doesn&apos;t mind.
+I meant to have more comprehensive instructions but haven&apos;t gotten to it. when he&apos;s outside watch what he eats. I tried to eat a screw last time :facepalm:
+Things you can do are: say his name if he looks at you give him a treat (i have shredded chicken) he kind of knows sit, laydown and o-te (shake) give him treats if he does it. He can play fetch if he&apos;s engaged and he loves tug of war.</p>
+      </main>
     </div>
   );
 }
